3fb0927f9f81f74fbd6ffb4e0adea94196102664
Rizky-Iqbal36 <rizkiiqbal36@gmail.com>
2024-03-07 18:03:33 +0700

    refactor: 💡 log app environment

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,8 +9,9 @@ import TransformInterceptor from '@app/interceptor';
 const logger = new Logger();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors();
   app.useGlobalInterceptors(new TransformInterceptor());
+  logger.debug(`| App environment: ${appConfig.app.env}`);
   await app.listen(appConfig.app.port, async () => {
     logger.debug(`🚀 Server ready at port: ${appConfig.app.port}`);
   });
